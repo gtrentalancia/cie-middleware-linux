845a40e8397c8524a7fb9269ffd3d684eb96e9d2
Guido Trentalancia <guido@trentalancia.com>
2022-06-21 21:47:27 +0100

    Add library file libjnidispatch.so from the JNA package to the CIEID jar file.

--- CIEID/src/create-jar-script.sh
+++ CIEID/src/create-jar-script.sh
@@ -6,5 +6,6 @@ IFS='
 '
 for i in `find . -type f -name "*.class"` ; do echo -n "$i" | sed 's/\ /\\\ /g' | sed 's/\$/\\\$/g' | sed 's/\@/\\@/g' >> create-jar.sh ; echo -n " " >> create-jar.sh ; done
 for i in `find ./it/ipzs/cieid/res/ -type f` ; do echo -n "$i" | sed 's/\ /\\\ /g' | sed 's/\$/\\\$/g' | sed 's/\@/\\@/g' >> create-jar.sh ; echo -n " " >> create-jar.sh ; done
+for i in `find ./com/sun/jna/ -type f -name libjnidispatch.so` ; do echo -n "$i" | sed 's/\ /\\\ /g' | sed 's/\$/\\\$/g' | sed 's/\@/\\@/g' >> create-jar.sh ; echo -n " " >> create-jar.sh ; done
 chmod a+x create-jar.sh
 [[ -e create-jar.sh ]] && ./create-jar.sh
